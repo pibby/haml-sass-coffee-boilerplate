04d568fa83e65c7d3855bf93d6539ce4bce3f43b
Katie Harron <katiedixon@gmail.com>
2015-02-07 12:02:43 -0600

    General tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('haml', function () {
 gulp.task('styles', function() {
 	return gulp.src(paths.scss)
 		.pipe(sass({ style: 'expanded' }))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'ios 6', 'android 4'))
 		.pipe(gulp.dest(paths.dist.css))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(minifycss())
