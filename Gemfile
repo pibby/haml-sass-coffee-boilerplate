6bfa96a58a2d89b453d2923cf959f49161b250f1
Katie Harron <katie@pibby.com>
2015-07-18 18:17:06 -0500

    Add minification for CSS and JS

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby '2.2.2'
 gem 'sinatra'
 gem 'sinatra-asset-pipeline'
 gem 'haml'
+gem 'sass'
+gem 'uglifier'
 gem 'eventmachine', '>=1.0.4'
 
 group :development do
--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
+# CSS minification
+  set :assets_css_compressor, :sass
+
+  # JavaScript minification
+  set :assets_js_compressor, :uglifier
+
   register Sinatra::AssetPipeline
 
   get '/' do
